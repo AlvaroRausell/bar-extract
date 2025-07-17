7267bdee71f1202ed526bdbe513a759e009f0d23
Alvaro Rausell Guiard <alvarorausell@github.com>
2025-07-17 15:06:35 +0000

    Fix postCreateCommand path and ensure ACT is moved to the correct directory

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Install ACT for GitHub Actions local testing
 curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
+sudo mv ./bin/act /usr/local/bin
